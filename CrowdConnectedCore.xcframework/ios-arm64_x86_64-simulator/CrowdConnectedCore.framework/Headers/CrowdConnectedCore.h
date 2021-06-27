2f596fe82a603c884a2150d0cad04ebb761e7412
Robert Tanase <robert.tanase97@gmail.com>
2021-06-27 10:58:33 +0100

    Add CrowdConnectedCore XCFramework

---
+++ CrowdConnectedCore.xcframework/ios-arm64_x86_64-simulator/CrowdConnectedCore.framework/Headers/CrowdConnectedCore.h
@@ -0,0 +1,15 @@
+/*
+  CrowdConnectedCore.h
+  CrowdConnectedCore
+
+  Created by TCode on 13/02/2021.
+  Copyright © 2021 Crowd Connected. All rights reserved.
+*/
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CrowdConnectedCore.
+FOUNDATION_EXPORT double CrowdConnectedCoreVersionNumber;
+
+//! Project version string for CrowdConnectedCore.
+FOUNDATION_EXPORT const unsigned char CrowdConnectedCoreVersionString[];
