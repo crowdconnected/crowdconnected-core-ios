6ddee2b83c2f1393afb056ba24a5a30bafe6b7de
Robert Tanase <robert.tanase97@gmail.com>
2021-07-09 17:01:27 +0300

    Update package

--- Package.swift
+++ Package.swift
@@ -7,37 +7,34 @@ let package = Package(
         .iOS(.v12)
     ],
     products: [
-        .library(
-            name: "CrowdConnectedCore",
-            targets: ["CrowdConnectedCoreTarget"])
+        .library(name: "CrowdConnectedCore",
+                 targets: ["CrowdConnectedCoreTarget"])
     ],
     dependencies: [
         .package(name: "CrowdConnectedShared",
                  url: "https://github.com/crowdconnected/crowdconnected-shared-ios.git",
-                 from: "1.0.0"),
+                 from: "1.0.1"),
         .package(name: "AWS-SDK",
                  url: "https://github.com/aws-amplify/aws-sdk-ios-spm",
                  from: "2.24.1")
     ],
     targets: [
         .target(name: "CrowdConnectedCoreTarget",
-                dependencies: [.target(name: "CrowdConnectedCoreWrapper")],
-                path: "CrowdConnectedCoreWrapper"
-        ),
-        .target(
-            name: "CrowdConnectedCoreWrapper",
-            dependencies: [
-                .target(name: "CrowdConnectedCore"),
-                .product(name: "CrowdConnectedShared", package: "CrowdConnectedShared"),
-                .product(name: "AWSCognitoIdentityProvider", package: "AWS-SDK"),
-                .product(name: "AWSCognitoIdentityProviderASF", package: "AWS-SDK"),
-                .product(name: "AWSCore", package: "AWS-SDK")
-            ],
-            path: "CrowdConnectedCoreWrapper",
-        ),
-        .binaryTarget(
-            name: "CrowdConnectedCore",
-            path: "CrowdConnectedCore.xcframework",
-        ),
+                dependencies: [
+                    .target(name: "CrowdConnectedCoreWrapper")
+                ],
+                path: "CrowdConnectedCoreWrapper"),
+        .target(name: "CrowdConnectedCoreWrapper",
+                dependencies: [
+                    .target(name: "CrowdConnectedCore"),
+                    .product(name: "CrowdConnectedShared", package: "CrowdConnectedShared"),
+                    .product(name: "AWSCognitoIdentityProvider", package: "AWS-SDK"),
+                    .product(name: "AWSCognitoIdentityProviderASF", package: "AWS-SDK"),
+                    .product(name: "AWSCore", package: "AWS-SDK")
+                ],
+                path: "CrowdConnectedCoreWrapper"),
+        .binaryTarget(name: "CrowdConnectedCore",
+                      path: "CrowdConnectedCore.xcframework"
+        )
     ]
 )
