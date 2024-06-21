4f6a5bf0fd703837c30f20ba45de4aae320e4fc6
Robert Tanase <robert.tanase97@gmail.com>
2024-06-21 14:31:33 +0300

    Version 1.6.5

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/crowdconnected/crowdconnected-shared-ios.git",
-                 from: "1.6.4"),
+                 from: "1.6.5"),
         .package(url: "https://github.com/aws-amplify/aws-sdk-ios-spm",
                  from: "2.28.0")
     ],
